36902e8add32ade646c3d62cb3e5b8b2bfd87126
Rik Rose <rikrose@gmail.com>
2024-05-06 20:51:58 -0700

    update tags

--- .eleventy.js
+++ .eleventy.js
@@ -8,14 +8,14 @@ const markdownItOptions = {
 }
 
 function tierTable(content){
- return '<table class="tierTable">' + content + '</table>';
+ return '<table class="tier-table">' + content + '</table>';
 }
 
 function tierTableRow(content, rowType){
     if (!['s', 'a', 'b', 'c', 'd', 'f'].includes(rowType)){
         return "ERROR - Bad rowType - not one of s, a, b, c, d, f";
     }
-    return '<tr><td class="tier-' + rowType + '">' + rowType + "</td><td>" + content + '</td></tr>'
+    return '<tr class="tier-table-row"><td class="tier-' + rowType + '">' + rowType + "</td><td>" + content + '</td></tr>'
 }
 
 module.exports = function (eleventyConfig) {
