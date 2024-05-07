6ddd32fcc4408e22d0df94588ea002c176023f6d
Rik Rose <rikrose@gmail.com>
2024-05-06 19:26:15 -0700

    Test shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,33 @@
+const markdownIt = require('markdown-it');
+const markdownItAttrs = require('markdown-it-attrs');
+
+const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+}
+
+function tierTable(content){
+ return '<table class="tierTable">' + content + '</table>';
+}
+
+function tierTableRow(content, rowType){
+    if (!['s', 'a', 'b', 'c', 'd', 'f'].includes(rowType)){
+        return "ERROR - Bad rowType - not one of s, a, b, c, d, f";
+    }
+    return '<tr class=tier-"' + rowType + '">' + content + '</tr>'
+}
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
         files: './public/static/**/*.css',
     });
+
+    const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs);
+    eleventyConfig.setLibrary('md', markdownLib);
+
+    eleventyConfig.addPairedShortcode("TierTable", tierTable);
+    eleventyConfig.addPairedShortcode("TierTableRow", tierTableRow);
 
     return {
         dir: {
